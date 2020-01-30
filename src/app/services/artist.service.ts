871de0a7535c73fd3f4959dfd04095e18731d563
letizia obiglio <letizia.obiglio@gmail.com>
2020-01-30 12:13:08 +0100

    feat: artist service added

--- src/app/services/artist.service.ts
+++ src/app/services/artist.service.ts
@@ -1,6 +1,8 @@
+import { Artist } from './../models/artist';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 
 @Injectable({
@@ -12,5 +14,25 @@ export class ArtistService {
 
   constructor(private http: HttpClient) { }
 
-  // public gelAll(): Observable<
+  public getAll(): Observable<Artist[]> {
+    return this.http
+      .get(ArtistService.URL)
+      .pipe(map(this.convertDataFromServerToArtists));
+  }
+
+  public getByVotes(): Observable<Artist[]> {
+    return this.http
+      .get(ArtistService.URL + '/bests')
+      .pipe(map(this.convertDataFromServerToArtists));
+  }
+
+  private convertDataFromServerToArtists(artists: any[]): Artist[] {
+    return artists.map(artist => new Artist(artist));
+  }
+
+  public getById(id: number): Observable<Artist> {
+    return this.http
+      .get(ArtistService.URL + '/' + id)
+      .pipe(map((artist: Artist) => new Artist(artist)));
+  }
 }
