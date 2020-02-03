b9e7fe2b8a4dbb088d24eac17e8f0c787f7a3b6f
letizia obiglio <letizia.obiglio@gmail.com>
2020-02-03 13:23:46 +0100

    feat: artists part done

--- src/app/pages/artist/artist.component.ts
+++ src/app/pages/artist/artist.component.ts
@@ -9,22 +9,35 @@ import { DomSanitizer } from '@angular/platform-browser';
   styleUrls: ['./artist.component.scss']
 })
 export class ArtistComponent implements OnInit {
-  artistsByVote: Artist[] = [];
-  bestArtist: Artist = new Artist();
 
   constructor(private artistService: ArtistService,
     // tslint:disable-next-line: align
     public sanitizer: DomSanitizer
   ) { }
+  artistsByVote: Artist[] = [];
+  bestArtist: Artist = new Artist();
+  artists: Artist[] = [];
+  showArtist: Artist;
 
   ngOnInit() {
     this.artistService.getByVotes().subscribe((artistsByVote) => {
       this.artistsByVote = artistsByVote;
       this.bestArtist = artistsByVote[0];
     });
+    this.artistService.getAll().subscribe((artists) => {
+      this.artists = artists;
+    });
   }
 
   // videoURL() {
   //   return this.sanitizer.bypassSecurityTrustResourceUrl(this.bestArtist.videoUrl);
   // }
+
+  artistCliqued(artist: Artist) {
+    this.showArtist = artist;
+  }
+
+  addVote() {
+
+  }
 }
